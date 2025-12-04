7d98113b25270f9ebd8933cc6bd26bdabd7d1eaa
Garcia <731366@student.dusd.net>
2025-12-04 13:14:30 -0800

    re added project
    Should be current now

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        //currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = MoveAction.ReadValue<Vector2>();
+        
+    }
+
+
+     void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+
+     public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+}
